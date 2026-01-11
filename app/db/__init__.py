3f93ca9fef9d4fdce8ab614ddf1a2cf005989087
ゆんふぃ <yunfie.twitter+3@gmail.com>
2026-01-11 17:28:07 +0900

    feat: Create app/db/__init__.py - Database module exports

--- app/db/__init__.py
+++ app/db/__init__.py
@@ -1,1 +1,17 @@
-"""Database module initialization."""
+"""Database module - Models and migrations."""
+
+from app.db.models import (
+    CrawlSession,
+    CrawlJob,
+    CrawlMetadata,
+    PageAnalysis,
+    SearchContent,
+)
+
+__all__ = [
+    "CrawlSession",
+    "CrawlJob",
+    "CrawlMetadata",
+    "PageAnalysis",
+    "SearchContent",
+]
