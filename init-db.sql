64878704effb7336987c4af9a6645602a22922e8
ゆんふぃ <yunfie.twitter+3@gmail.com>
2026-01-12 18:28:05 +0900

    fix(db): improve PostgreSQL initialization script with better error handling

--- init-db.sql
+++ init-db.sql
@@ -1,11 +1,22 @@
--- Create search_user role if it doesn't exist
-CREATE ROLE search_user WITH LOGIN PASSWORD 'search_password';
+-- PostgreSQL Initialization Script
+-- This script will be run by the postgres user during container initialization
 
--- Grant privileges
+-- Create search_user role if it doesn't exist
+DO $$ 
+BEGIN
+  IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'search_user') THEN
+    CREATE ROLE search_user WITH LOGIN PASSWORD 'search_password';
+    RAISE NOTICE 'Created role: search_user';
+  ELSE
+    RAISE NOTICE 'Role search_user already exists';
+  END IF;
+END $$;
+
+-- Grant CREATEDB privilege to search_user
 ALTER ROLE search_user CREATEDB;
 
--- Create database if it doesn't exist
-CREATE DATABASE transparent_search OWNER search_user;
+-- Grant connection to search_user on postgres database
+GRANT CONNECT ON DATABASE postgres TO search_user;
 
--- Grant all privileges on the database
+-- Grant privileges to search_user on transparent_search database
 GRANT ALL PRIVILEGES ON DATABASE transparent_search TO search_user;
